333e2a49aeccb8776317581add4344eeabb76d61
sampatirage <43390550+sampatirage@users.noreply.github.com>
2019-06-12 21:02:59 +1000

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -12,17 +12,18 @@ namespace XREChall
         {
 
             decimal [] price = { 2.90m, 5.40m, 8.90m };
-            int[] noOfItemsInPacks = { 2, 5, 8 };     // Pack size A, B, C
-            int[] noOfPacksOrdered = { 0, 0, 0 };   // initiate only
+            int[] noOfItemsInPacks = { 2, 5, 0 };     // Pack size A, B, C
+            int[] noOfPacksOrdered = { 100, 100, 100 };   // initiate with a large number 
             int[] maxNoOfPacksEach = { 0, 0, 0 };  // initiate only
 
+            Console.WriteLine(" No of Items in each pack1-{0}, pack2-{1}, pack3-{2}", noOfItemsInPacks[0], noOfItemsInPacks[1], noOfItemsInPacks[2]);
             Console.WriteLine(" Enter total number required ");
             int totalRequiredNumber =int.Parse( Console.ReadLine());
 
             // Calc Max number of packs of each pack by dividing total number by No in each pack
             for (int i = 0; i < noOfPacksOrdered.Length; i++)
             {
-                maxNoOfPacksEach[i] = totalRequiredNumber / noOfItemsInPacks[i];
+                maxNoOfPacksEach[i] = noOfItemsInPacks[i]>0 ? totalRequiredNumber / noOfItemsInPacks[i]:0 ;
                 Console.Write(" pack {0}, Needs {1} packs ", i, maxNoOfPacksEach[i]);                
             }
             Console.WriteLine();
@@ -43,13 +44,23 @@ namespace XREChall
                         int grandTotal = totalQtyInApacks + totalQtyInBpacks + totalQtyInCpacks;
                         if (grandTotal == totalRequiredNumber)
                         {
+                            int totalNumberOfPacks = i + j + k;
+                            // if it is less than the already selected option, select this as the new pack option
+                            if (totalNumberOfPacks < noOfPacksOrdered[0]+ noOfPacksOrdered[1]+noOfPacksOrdered[2] )
+                            {
+                                noOfPacksOrdered[0] = i;
+                                noOfPacksOrdered[1] = j;
+                                noOfPacksOrdered[2] = k;
+                            }
                             Console.Write(" pack-A {0} x pack-B {1} pack-C {2} ", i, j, k);
 
                             Console.WriteLine("  A {0} + B {1}+ C {2},  Total {3}", totalQtyInApacks, totalQtyInBpacks, totalQtyInCpacks, grandTotal);
                         }
                     }
                 }
-            }          
+            }
+
+            Console.WriteLine("Selected pack combination {0}, {1}, {2}", noOfPacksOrdered[0], noOfPacksOrdered[1], noOfPacksOrdered[2]);
 
             Console.Read();
         }
