2a2b6e105435807ae4341d803dd02c586f4cab75
sampatirage <43390550+sampatirage@users.noreply.github.com>
2019-06-14 20:14:13 +1000

    Add files via upload

--- CartItem.cs
+++ CartItem.cs
@@ -11,10 +11,11 @@ namespace XREChall
         public int[] NoOfItemsInPacks { get; set; }
         public decimal[] Price { get; set; }
         public string ProductCode { get; set; }
-        int[] noOfPacksToOrder = { 100, 100, 100 };   // initiate with a large number 
+        static int MAXNOOFPACKS = 100;
+        int[] noOfPacksToOrder = { MAXNOOFPACKS, MAXNOOFPACKS, MAXNOOFPACKS };   // initiate with a large number 
 
 
-        // Constructor 1
+        // Constructor -No argument 
         public CartItem()
         {
         }
@@ -24,49 +25,50 @@ namespace XREChall
             NoOfItemsInPacks = noOfItemsInPacks;
             Price = price;
             ProductCode = productCode;
-            Console.WriteLine("product Code" + ProductCode);
-        }   
+        }
 
-        
-       public override string  ToString()
+
+        public override string ToString()
         {
             string returnString = null;
-            for (int i=0; i< NoOfItemsInPacks.Length; i++)
+            for (int i = 0; i < NoOfItemsInPacks.Length; i++)
             {
-                returnString += (NoOfItemsInPacks[i] + " @ $" + Price[i] +", ");
+                returnString += (NoOfItemsInPacks[i] + " @ $" + Price[i] + ", ");
             }
 
             return returnString;
         }
 
 
-       public int[] GetOptiumNoOfPacksSingleItem( int totalRequiredNumber1)
+        public int[] GetOptiumNoOfPacksSingleItem(int totalRequiredNumber1)
         {
             int[] maxNoOfPacksEach = { 0, 0, 0 };  // initiate only
 
-            // Calc Max number of packs of each pack by dividing total number by No in each pack
+            // Calc Max number of packs of each pack by dividing total required number by No in each pack
+            //  That way we do not need to iterate beyond the maxNoOfPacksEach
             for (int i = 0; i < noOfPacksToOrder.Length; i++)
             {
                 maxNoOfPacksEach[i] = NoOfItemsInPacks[i] > 0 ? totalRequiredNumber1 / NoOfItemsInPacks[i] : 0;
-                Console.Write(" pack {0}, Max {1} packs ", i, maxNoOfPacksEach[i]);
+               // Console.Write(" pack {0}, Max {1} packs ", i, maxNoOfPacksEach[i]);
             }
             Console.WriteLine();
 
-            // Calculate for each combination of noOfPacksOrdered
+            // Calculate for each combination of noOfPacksOrdered, and find the minimum no of packs required.
 
             for (int i = 0; i <= maxNoOfPacksEach[0]; i++)  // pack size A
             {
-                int totalQtyInApacks = NoOfItemsInPacks[0] * i;
+                int totalQtyInApacks = NoOfItemsInPacks[0] * i;  // Add i number of packs from this pack size
 
                 for (int j = 0; j <= maxNoOfPacksEach[1]; j++) // pack size B
                 {
-                    int totalQtyInBpacks = NoOfItemsInPacks[1] * j;
+                    int totalQtyInBpacks = NoOfItemsInPacks[1] * j; // Add j number of packs from this pack size
 
                     for (int k = 0; k <= maxNoOfPacksEach[2]; k++) // pack size C
                     {
-                        int totalQtyInCpacks = NoOfItemsInPacks[2] * k;
-                        int grandTotal = totalQtyInApacks + totalQtyInBpacks + totalQtyInCpacks;
-                        if (grandTotal == totalRequiredNumber1)
+                        int totalQtyInCpacks = NoOfItemsInPacks[2] * k; // Add k number of packs from this pack size
+
+                        int grandTotal = totalQtyInApacks + totalQtyInBpacks + totalQtyInCpacks;  // total in this iteration
+                        if (grandTotal == totalRequiredNumber1) // Does it match our need.
                         {
                             int totalNumberOfPacks = i + j + k;
                             // if it is less than the already selected option, select this as the new pack option
@@ -76,9 +78,9 @@ namespace XREChall
                                 noOfPacksToOrder[1] = j;
                                 noOfPacksToOrder[2] = k;
                             }
-                            Console.Write(" pack-A {0} x pack-B {1} pack-C {2} ", i, j, k);
+                            // Console.Write(" pack-A {0} x pack-B {1} pack-C {2} ", i, j, k);
 
-                            Console.WriteLine("  A {0} + B {1}+ C {2},  Total {3}", totalQtyInApacks, totalQtyInBpacks, totalQtyInCpacks, grandTotal);
+                            // Console.WriteLine("  A {0} + B {1}+ C {2},  Total {3}", totalQtyInApacks, totalQtyInBpacks, totalQtyInCpacks, grandTotal);
                         }
                     }
                 }
@@ -95,10 +97,14 @@ namespace XREChall
             decimal totalPrice = 0m;
             for (int i = 0; i < NoOfItemsInPacks.Length; i++)
             {
-                if (noOfPacksToOrder[i] > 0)
+                if (noOfPacksToOrder[i] > 0 && noOfPacksToOrder[i] < MAXNOOFPACKS)
                 {             // Print if no of packs is > 0
                     Console.WriteLine(" {0}x{1} packs. @ Price ${2} ", noOfPacksToOrder[i], NoOfItemsInPacks[i], Price[i]);
                     totalPrice += noOfPacksToOrder[i] * Price[i];
+                }
+                else
+                {
+                    Console.WriteLine(" Something went wrong, May be we cannot provide the no of packs to match quantity");
                 }
             }
             Console.WriteLine(" Total price =" + totalPrice + "\n");
--- Program.cs
+++ Program.cs
@@ -22,121 +22,95 @@ namespace XREChall
             int[] noOfItemsInPacksSH3 = { 3, 5, 0 };     // Pack size A, B, C    
             decimal[] priceSH3 = { 2.99m, 4.49m, 0.0m };
             string productCodeSH3 = "SH3";
-
-
             cartItems[0].NoOfItemsInPacks = noOfItemsInPacksSH3;
             cartItems[0].Price = priceSH3;
-            cartItems[0].ProductCode = productCodeSH3;
-
-            //  Console.WriteLine(" to String " + SH3.ToString());
-
-            //Console.WriteLine("\n No of SH3 Items in each pack1-{0}, pack2-{1}, pack3-{2}",
-            //       noOfItemsInPacksSH3[0], noOfItemsInPacksSH3[1], noOfItemsInPacksSH3[2]);
-
-
-            //  int[] noOfSH3PacksToShip = GetOptiumNoOfPacksSingleItem(noOfItemsInPacksSH3, totalRequiredNumberSH3);
-            //  Console.WriteLine("Selected pack combination {0}, {1}, {2}", noOfSH3PacksToShip[0], noOfSH3PacksToShip[1], noOfSH3PacksToShip[2]);
-
-            //  PrintPackDetails(noOfItemsInPacksSH3, priceSH3, noOfSH3PacksToShip, productCodeSH3);
+            cartItems[0].ProductCode = productCodeSH3;           
 
             #endregion
-          
+
             // YT2
             #region: YT2
+
             int[] noOfItemsInPacksYT2 = { 4, 10, 15 };     // Pack size A, B, C  
             decimal[] priceYT2 = { 4.95m, 9.95m, 13.95m };
             string productCodeYT2 = "YT2";
-
             cartItems[1].NoOfItemsInPacks = noOfItemsInPacksYT2;
             cartItems[1].Price = priceYT2;
             cartItems[1].ProductCode = productCodeYT2;
-
-
-            //Console.WriteLine("\n No of YT2 Items in each pack1-{0}, pack2-{1}, pack3-{2}",
-            //  noOfItemsInPacksYT2[0], noOfItemsInPacksYT2[1], noOfItemsInPacksYT2[2]);
-            //Console.Write(" Enter total number required YT2   ");
-            // int totalRequiredNumberYT2 = int.Parse(Console.ReadLine());
-
-            // int[] noOfYT2PacksToShip = GetOptiumNoOfPacksSingleItem(noOfItemsInPacksYT2, totalRequiredNumberYT2);
-            // Console.WriteLine("Selected pack combination {0}, {1}, {2}", noOfYT2PacksToShip[0], noOfYT2PacksToShip[1], noOfYT2PacksToShip[2]);
-
-            //PrintPackDetails(noOfItemsInPacksYT2, priceYT2, noOfYT2PacksToShip, productCodeYT2);
-
+            
             #endregion
 
             #region TR
-            
-           int[] noOfItemsInPacksTR = { 3, 5, 9 };     // Pack size A, B, C   
-           decimal[] priceTR = { 2.95m, 4.45m, 7.99m };
-           string productCodeTR = "TR";
+
+            int[] noOfItemsInPacksTR = { 3, 5, 9 };     // Pack size A, B, C   
+            decimal[] priceTR = { 2.95m, 4.45m, 7.99m };
+            string productCodeTR = "TR";
 
             cartItems[2].NoOfItemsInPacks = noOfItemsInPacksTR;
             cartItems[2].Price = priceTR;
             cartItems[2].ProductCode = productCodeTR;
 
-            // Console.WriteLine("\n No of TR Items in each pack1-{0}, pack2-{1}, pack3-{2}",
-            //    noOfItemsInPacksTR[0], noOfItemsInPacksTR[1], noOfItemsInPacksTR[2]);
-            //Console.Write(" Enter total number required TR   ");
-            //int totalRequiredNumberTR = int.Parse(Console.ReadLine());
-
-            //int[] noOfTRPacksToShip = GetOptiumNoOfPacksSingleItem(noOfItemsInPacksTR, totalRequiredNumberTR);
-            //Console.WriteLine("Selected pack combination {0}, {1}, {2}", noOfTRPacksToShip[0], noOfTRPacksToShip[1], noOfTRPacksToShip[2]);
-
-            // PrintPackDetails(noOfItemsInPacksTR, priceTR, noOfTRPacksToShip, productCodeTR);
-
             #endregion
-
-            // Yoghurt YT2 4 @ $4.95, 10 @ $9.95, 15 @ $13.95
-
 
             int[] noOfItemsRequired = { 0, 0, 0 }; // initiate array of required no of items
             string[] codeOfItemsRequired = { "", "", "" };  // ProductCode
 
-            string inputS;  // Read quantiy and code from keyboard
-            int cartItemSequenNumberMatch = -1;  // Matching sequence number from Product Array 
+            string inputS;  // To read quantiy and code from keyboard
+            int cartItemSequenNumberMatch = -1;  // Initialise as negative, Matching sequence number from Product Array 
 
             for (int i = 0; i < noOfItemsRequired.Length; i++)
             {
-                Console.Write(" Enter number of items followed by code   ");
-                inputS = Console.ReadLine();
+                Console.Write(" Enter number of items followed by space then code (Ex : 12 SH3)   ");
+                inputS = Console.ReadLine().Trim();  // Get input from screen
+
+                if (inputS.Length < 3)  // Length has to be at least 3 
+                {
+                    break;
+                }
                 string[] inputDetail = inputS.Split(' ');
                 try
                 {
-                    noOfItemsRequired[i] = int.Parse(inputDetail[0]);
-                    codeOfItemsRequired[i] = inputDetail[1];
+                    noOfItemsRequired[i] = int.Parse(inputDetail[0]); 
+                    codeOfItemsRequired[i] = inputDetail[1].ToUpper();
                 }
                 catch // if input is not in proper format set them to 0 and ""
                 {
                     noOfItemsRequired[i] = 0; ;
                     codeOfItemsRequired[i] = "";
                 }
-                Console.WriteLine(" no required " + noOfItemsRequired[i] + " Code =" + codeOfItemsRequired[i]);
+               // Console.WriteLine(" no required " + noOfItemsRequired[i] + " Code =" + codeOfItemsRequired[i]);
 
                 // Find the matching item by searching product code
-               
-                if (noOfItemsRequired[i] > 0 )
+                bool matchFound = false;
+                if (noOfItemsRequired[i] > 0)
                 {
-                    for (int j =0; j< cartItems.Length; j++)
+                    for (int j = 0; j < cartItems.Length; j++)
                     {
                         if (cartItems[j].ProductCode.Equals(codeOfItemsRequired[i]))
                         {
+                            matchFound = true;
                             cartItemSequenNumberMatch = j; // matching item found from product codes
                             break;
                         }
                     }
                 }
-                Console.WriteLine(" CartItem " + cartItemSequenNumberMatch + " matches code " + codeOfItemsRequired[i]);
-
-                int[] noOfPacksToShip =
-               cartItems[cartItemSequenNumberMatch].GetOptiumNoOfPacksSingleItem(noOfItemsRequired[i]);
-
-                cartItems[cartItemSequenNumberMatch].PrintPackDetails();
+                if (matchFound)
+                {
+                   // Console.WriteLine(" CartItem " + cartItemSequenNumberMatch + " matches code " + codeOfItemsRequired[i]);
+                    int[] noOfPacksToShip =
+                    cartItems[cartItemSequenNumberMatch].GetOptiumNoOfPacksSingleItem(noOfItemsRequired[i]);
+                    cartItems[cartItemSequenNumberMatch].PrintPackDetails();
+                }
+                else
+                {
+                    Console.WriteLine(" Error, no matching item found for the given code or other error ");
+                }
 
             } // end of for i
 
 
 
-           
+
 
 
             Console.Read();
